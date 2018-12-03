45fef5cc607e538a5d5a47b48c87ac907a94abdb
rulamon <ruben.lamon@gmail.com>
2018-12-03 09:07:41 +0100

    fixed iteration bug with empty array as element

--- 2.js
+++ 2.js
@@ -1,13 +1,16 @@
 function steamrollArray(arr) {
+	//function to test if argument is an array
 	const test = el => Array.isArray(el);
-	const insert = (array, index) => array = array.slice(0, index).concat(steamrollArray(array[index])).concat(array.slice(index+1));
+	//loop the array, replacing any element that is an array by its contents a level deeper, and use recursion
+	//to flatten the element further if necessary
 	for (let i = 0; i < arr.length; i++) {
 		if(test(arr[i])) {
-			//arr = arr.slice(0, i).concat(steamrollArray(arr[i])).concat(arr.slice(i+1));
-			insert(arr, i);
+			let origLength = arr.length
+			arr = arr.slice(0, i).concat(steamrollArray(arr[i])).concat(arr.slice(i+1));
+			if (arr.length < origLength) {i--}
 		}
 	}
 	return arr;
 }
 
-console.log(steamrollArray([1, {}, [3, [[4]]]]))
+console.log(steamrollArray([1, [], [3, [[4]]]]))
--- 3.js
+++ 3.js
@@ -4,11 +4,15 @@ function steamrollArray(arr) {
 		.concat(steamrollArray(array[index])).concat(array.slice(index+1));
 	for (let i = 0; i < arr.length; i++) {
 		if(test(arr[i])) {
+			let origLength = arr.length
 			//arr = arr.slice(0, i).concat(steamrollArray(arr[i])).concat(arr.slice(i+1));
 			insert(arr, i);
+			if (arr.length < origLength) {i--}
+
+
 		}
 	}
 	return arr;
 }
 
-console.log(steamrollArray([1, {}, [3, [[4]]]]))
+console.log(steamrollArray([1, [], [3, [[4]]]]))
