26a8f159c8e0bc1dd07d87c44603c9c97ce888be
rulamon <ruben.lamon@gmail.com>
2018-12-03 08:08:57 +0100

    added comments

--- 1.js
+++ 1.js
@@ -1,8 +1,11 @@
 function steamrollArray(arr) {
+	//create function to test if an element is an array and assign it to variable test
 	const test = el => Array.isArray(el);
+	//use a while loop and nested for loop to keep iterating the original array as long as some elements are arrays
 	while (arr.some(el => test(el))) {
 		for (let i = 0; i < arr.length; i++) {
 			if(test(arr[i])) {
+				//flatten the element using concat
 				arr = arr.slice(0, i).concat(arr[i]).concat(arr.slice(i+1))
 			}
 		}
