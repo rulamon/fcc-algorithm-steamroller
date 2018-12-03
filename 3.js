57b7bb4b26e052e49b43b6767cebe5b7f22f984b
rulamon <ruben.lamon@gmail.com>
2018-12-03 08:35:10 +0100

    defined function before loop

---
+++ 3.js
@@ -0,0 +1,14 @@
+function steamrollArray(arr) {
+	const test = el => Array.isArray(el);
+	const insert = (array, index) => arr = array.slice(0, index)
+		.concat(steamrollArray(array[index])).concat(array.slice(index+1));
+	for (let i = 0; i < arr.length; i++) {
+		if(test(arr[i])) {
+			//arr = arr.slice(0, i).concat(steamrollArray(arr[i])).concat(arr.slice(i+1));
+			insert(arr, i);
+		}
+	}
+	return arr;
+}
+
+console.log(steamrollArray([1, {}, [3, [[4]]]]))
